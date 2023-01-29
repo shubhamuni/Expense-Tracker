6b5888b12fefcd6ab80087c8daa05dfb7fdc04cc
shubhampatil.ani@gmail.com <shubhampatil.ani@gmail.com>
2023-01-29 14:53:26 +0530

    Arrow function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
-import ExpenseItem from "./components/ExpenseItem";
-function App() {
+import Expenses from "./components/Expense/Expenses";
+import "./App.css";
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -32,33 +33,10 @@ function App() {
     },
   ];
   return (
-    <div>
-      <h2>Let's get started</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-        expenditure={expenses[0].expenditure}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-        expenditure={expenses[1].expenditure}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-        expenditure={expenses[2].expenditure}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-        expenditure={expenses[3].expenditure}
-      ></ExpenseItem>
+    <div className="div">
+      <h2>Let's get Started</h2>
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 export default App;
