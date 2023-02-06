f665c0720fd4c1b88374730fb64e4b1c5b9899c6
shubhampatil.ani@gmail.com <shubhampatil.ani@gmail.com>
2023-02-06 09:42:43 +0530

    filterYear

--- my-app/src/components/Expense/Expenses.js
+++ my-app/src/components/Expense/Expenses.js
@@ -1,44 +1,34 @@
+import React, { useState } from "react";
+
 import ExpenseItem from "./ExpenseItem";
-import "../UI/Card.css";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  const filterList = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
-    <div className="expense">
-      <Card>
-        {props.items.map((expense) => (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {filterList.map((expense) => (
           <ExpenseItem
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
           />
         ))}
-
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-          expenditure={props.items[0].expenditure}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-          expenditure={props.items[1].expenditure}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-          expenditure={props.items[2].expenditure}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-          expenditure={props.items[3].expenditure}
-        />
       </Card>
     </div>
   );
